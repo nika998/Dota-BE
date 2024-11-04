73c0e656b41f8fe4028d9470699beec765b5b533
nidulovic <dulovicnika27@gmail.com>
2024-11-04 20:00:14 +0100

    Job that calls actuator health-check endpoint

--- dota/src/main/java/com/artigo/dota/DotaApplication.java
+++ dota/src/main/java/com/artigo/dota/DotaApplication.java
@@ -3,7 +3,9 @@ package com.artigo.dota;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableScheduling
@@ -11,6 +13,11 @@ public class DotaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DotaApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
---
+++ dota/src/main/java/com/artigo/dota/job/ScheduledHealthCheck.java
@@ -0,0 +1,32 @@
+package com.artigo.dota.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@Slf4j
+public class ScheduledHealthCheck {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${app.base-url}")
+    private String baseUrl;
+
+    public ScheduledHealthCheck(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Scheduled(fixedRateString = "${app.health-check.rate}")
+    public void checkHealth() {
+        try {
+            String url = baseUrl + "/dota/actuator/health";
+            String response = restTemplate.getForObject(url, String.class);
+            log.info("Health Check Response: " + response);
+        } catch (Exception e) {
+            log.error("Health Check Failed: " + e.getMessage());
+        }
+    }
+}
